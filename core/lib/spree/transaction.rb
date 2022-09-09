4e7cde5805f442e0413d554aa4b813fce71f8749
Marc Busqué <marc@lamarciana.com>
2022-09-09 14:02:07 +0200

    Steps can be anything responding to call

--- core/lib/spree/transaction.rb
+++ core/lib/spree/transaction.rb
@@ -39,7 +39,7 @@ module Spree
       end
 
       def [](step, input)
-        result = Result.success(input).bind(&registry[step])
+        result = Result.success(input).bind(&registry[step].method(:call))
         if result.failure?
           throw :halt, result
         else
--- core/spec/lib/transaction_spec.rb
+++ core/spec/lib/transaction_spec.rb
@@ -75,4 +75,26 @@ describe Spree::Transaction do
 
     expect { instance.call(1).error! }.not_to raise_error
   end
+
+  it 'steps can be anything responding to call' do
+    one = Class.new do
+      def call(input)
+        Spree::Result.success(input + 1)
+      end
+    end.new
+    two = ->(x) { Spree::Result.success(x + 2) }
+    registry = {}
+    registry[:one] = one
+    registry[:two] = two
+    instance = Class.new do
+      include Spree::Transaction[registry: registry]
+
+      transaction do |input, t|
+        result_one = t[:one, input]
+        t[:two, result_one]
+      end
+    end.new
+
+    expect(instance.call(1).result!).to be(4)
+  end
 end
