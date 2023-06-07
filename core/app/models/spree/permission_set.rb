81d1dee76b13b1380a212ca5b2874352ef5124cc
Rainer Dema <rainer.dema@gmail.com>
2023-06-07 13:41:49 +0200

    Enhance Role model for better role management
    This commit expands the functionality of the `Spree::Role` model to provide more
    granular and efficient control over user roles and permissions.
    
      * Spree::RolePermission
      * Spree::Role enhancements
      * Spree::PermissionSet
    
    These enhancements are a part of our broader initiative to improve the
    flexibility and extensibility of role and permission management in Solidus,
    adapting functionality from the `solidus_user_roles` gem.

---
+++ core/app/models/spree/permission_set.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Spree
+  class PermissionSet < Spree::Base
+    has_many :role_permissions
+    has_many :roles, through: :role_permissions
+
+    validates :name, :set, presence: true
+
+    scope :display_permissions, -> { where('name LIKE ?', '%Display') }
+    scope :management_permissions, -> { where('name LIKE ?', '%Management') }
+
+    scope :custom_permissions, -> {
+      where.not(id: display_permissions)
+      .where.not(id: management_permissions)
+    }
+  end
+end
--- core/app/models/spree/role.rb
+++ core/app/models/spree/role.rb
@@ -3,12 +3,26 @@
 module Spree
   class Role < Spree::Base
     has_many :role_users, class_name: "Spree::RoleUser", dependent: :destroy
+    has_many :role_permissions, dependent: :destroy
+    has_many :permission_sets, through: :role_permissions
     has_many :users, through: :role_users
 
+    scope :non_base_roles, -> { where.not(name: ['admin']) }
+
     validates_uniqueness_of :name, case_sensitive: true
+    validates :name, uniqueness: true
+    after_save :assign_permissions
 
     def admin?
       name == "admin"
+    end
+
+    def permission_sets_constantized
+      permission_sets.map(&:set).map(&:constantize)
+    end
+
+    def assign_permissions
+      ::Spree::Config.roles.assign_permissions name, permission_sets_constantized
     end
   end
 end
