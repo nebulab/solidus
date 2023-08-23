9c97186ae63675e50dff50d3949f167c650e0b1e
Enzo Haguiwara <haguiwara68@gmail.com>
2023-08-23 17:26:09 -0300

    Load roles from database to RoleConfiguration

--- core/app/models/spree/role.rb
+++ core/app/models/spree/role.rb
@@ -7,7 +7,7 @@ module Spree
     has_many :permission_sets, through: :role_permissions
     has_many :users, through: :role_users
 
-    scope :non_base_roles, -> { where.not(name: ['admin']) }
+    scope :non_base_roles, -> { where.not(name: ['admin', 'default']) }
 
     validates_uniqueness_of :name, case_sensitive: true
     validates :name, uniqueness: true
